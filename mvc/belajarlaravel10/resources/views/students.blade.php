ff7d41cd79a6105ab7969aadecc0f953acfddd47
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-03-16 15:30:06 +0700

    Add laravel 10

---
+++ mvc/belajarlaravel10/resources/views/students.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel 10</title>
+</head>
+<body>
+    <center>
+        <h1>Halaman Pertama</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eum quam possimus maxime rerum. Mollitia eum velit harum accusamus. Debitis illo perferendis eligendi repudiandae sed soluta esse sunt excepturi labore!</p>
+    </center>
+</body>
+</html>
